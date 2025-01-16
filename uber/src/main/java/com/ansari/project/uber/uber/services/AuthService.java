6e695e550df0c990bfc0f2fad35ddf6454a08495
zeeshan <ansarizeeshan1232@gmail.com>
2025-01-16 18:35:25 +0530

    start writing the services

--- uber/src/main/java/com/ansari/project/uber/uber/services/AuthService.java
+++ uber/src/main/java/com/ansari/project/uber/uber/services/AuthService.java
@@ -1,4 +1,8 @@
 package com.ansari.project.uber.uber.services;
 
 public interface AuthService {
+
+    void login(String email, String Password);
+
+
 }
--- uber/src/main/java/com/ansari/project/uber/uber/services/DriverService.java
+++ uber/src/main/java/com/ansari/project/uber/uber/services/DriverService.java
@@ -1,4 +1,8 @@
 package com.ansari.project.uber.uber.services;
 
+import org.locationtech.jts.geom.Point;
+
 public interface DriverService {
+
+
 }
